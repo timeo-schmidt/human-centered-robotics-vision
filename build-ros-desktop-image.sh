57c8255940ae52808ca2b8d495294ecc9154079b
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-07 15:35:13 +0000

    fix

--- build-ros-desktop-image.sh
+++ build-ros-desktop-image.sh
@@ -18,4 +18,4 @@ docker build --build-arg ZED_SDK_MAJOR=${ZED_SDK_MAJOR} \
 --build-arg L4T_MAJOR_VERSION=${L4T_MAJOR_VERSION} \
 --build-arg L4T_MINOR_VERSION=${L4T_MINOR_VERSION} \
 --build-arg ROS_DISTRO_ARG=${ROS_DISTRO_ARG} \
--t "${TAG}" -f "${DOCKERFILE}" .
+-t "${TAG}" -f "${DOCKERFILE_PATH}" .
