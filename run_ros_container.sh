a31d0384977a37a1c72ec594e1ee0a120f11979b
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-13 18:09:17 +0000

    multilines

--- run_ros_container.sh
+++ run_ros_container.sh
@@ -4,4 +4,14 @@ source .env
 
 xhost +
 
-docker run --privileged --gpus all --entrypoint bash --runtime nvidia -it --net=host -e DISPLAY=$DISPLAY --ipc=host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket ${TAG}
+docker run \
+    --privileged \
+    --gpus all \
+    --entrypoint bash \
+    --runtime nvidia \
+    -it \
+    --net=host \
+    -e DISPLAY=$DISPLAY \
+    --ipc=host \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket \
+    ${TAG}
