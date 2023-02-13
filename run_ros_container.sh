053eeaf823bbd214ac32daa07018eb7b88606f7e
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-13 15:58:31 +0000

    gui docker run working

--- run_ros_container.sh
+++ run_ros_container.sh
@@ -4,14 +4,4 @@ source .env
 
 xhost +
 
-docker run \
-    -it  \
-    --privileged \
-    --gpus all \
-    --runtime nvidia \
-    --net=host \
-    -e DISPLAY=$DISPLAY \
-    --ipc=host \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket  \
-    ${TAG} \
-    bash
+docker run --privileged --gpus all --entrypoint bash --runtime nvidia -it --net=host -e DISPLAY=$DISPLAY --ipc=host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket ${TAG}
