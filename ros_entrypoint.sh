92de33062e59f15b2952935201d5fffd228e7270
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-13 14:49:11 +0000

    untested changes to files

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,6 +3,27 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" 
-source "$ROS_WS/devel/setup.bash"
+# source "$ROS_WS/devel/setup.bash"
+
+# setup catkin workspace
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+
+# Install zed sdk
+cd ~/catkin_ws/src
+git clone --recursive https://github.com/stereolabs/zed-ros-wrapper.git
+cd ../
+
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+
+catkin_make -DCMAKE_BUILD_TYPE=Release
+
+chmod a+x devel/setup.bash
+source ./devel/setup.bash
 
 exec "$@" # pass all arguments to the entrypoint
+
+# docker run --runtime nvidia --gpus all -it --privileged <DOCKER IMAGE>
