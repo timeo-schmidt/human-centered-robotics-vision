facd398e88cd9ff6f4cbb574fcd0a0787f0cf08a
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-13 17:22:16 +0000

    dockerfile update

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,27 +3,8 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" 
-# source "$ROS_WS/devel/setup.bash"
 
-# setup catkin workspace
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/
-catkin_make
-
-# Install zed sdk
-cd ~/catkin_ws/src
-git clone --recursive https://github.com/stereolabs/zed-ros-wrapper.git
-cd ../
-
-sudo rosdep init
-rosdep update
-rosdep install --from-paths src --ignore-src -r -y
-
-catkin_make -DCMAKE_BUILD_TYPE=Release
-
-chmod a+x devel/setup.bash
-source ./devel/setup.bash
-
-roslaunch zed_wrapper zed2i.launch
+# roslaunch zed_wrapper zed2i.launch
+roslaunch zed_display_rviz display_zed2i.launch    
 
 exec "$@" # pass all arguments to the entrypoint
