173b9710d9d617fcdec4b20e49c56f4204b8f00b
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-15 15:46:59 +0000

    change

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -6,6 +6,6 @@ source "/opt/ros/noetic/setup.bash"
 source "/opt/ros_ws/devel/setup.bash" 
 
 # roslaunch zed_wrapper zed2i.launch
-roslaunch zed_display_rviz display_zed2i.launch    
+# roslaunch zed_display_rviz display_zed2i.launch    
 
 exec "$@" # pass all arguments to the entrypoint
