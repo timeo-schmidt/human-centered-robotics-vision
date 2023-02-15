23e38df40cf06508b3803f2dcbdfa9a81eae920a
timeo-schmidt <timeo.schmidt@gmx.de>
2023-02-15 09:53:07 +0000

    fixed path

--- build-ros-desktop-image.sh
+++ build-ros-desktop-image.sh
@@ -9,8 +9,8 @@ docker build --build-arg ZED_SDK_MAJOR=${ZED_SDK_MAJOR} \
 --build-arg L4T_MAJOR_VERSION=${L4T_MAJOR_VERSION} \
 --build-arg L4T_MINOR_VERSION=${L4T_MINOR_VERSION} \
 --build-arg ROS_DISTRO_ARG=${ROS_DISTRO_ARG} \
--t "${TAG_STAGE_1}" -f "${DOCKERFILE_PATH}" .
+-t "${TAG_STAGE_1}" -f "${DOCKERFILE_STAGE_1}" .
 
 echo "Building '${TAG_STAGE_2}'"
 
-docker build --build-arg BASE_IMAGE="${TAG_STAGE_1}" -t "${TAG_STAGE_2}" -f "${DOCKERFILE_PATH}" .
+docker build --build-arg BASE_IMAGE="${TAG_STAGE_1}" -t "${TAG_STAGE_2}" -f "${DOCKERFILE_STAGE_2}" .
