63d1401ba01adb4a58cd6f4780b12f20295dd103
Timeo Schmidt <timeo.schmidt@gmx.de>
2023-03-06 17:09:50 +0000

    added local changes

--- aruco.sh
+++ aruco.sh
@@ -4,7 +4,4 @@ source /opt/ros/noetic/setup.bash
 source /opt/ros_ws/devel/setup.bash
 export ROS_IP=192.168.50.102
 export ROS_MASTER_URI=http://192.168.50.101:11311
-#roslaunch zed_wrapper zed2i.launch
-#apt-get install --fix-missing -y ros-noetic-aruco-ros
-#./aruco.sh &
-roslaunch zed_rtabmap_example zed_rtabmap.launch
+roslaunch aruco_ros single.launch
